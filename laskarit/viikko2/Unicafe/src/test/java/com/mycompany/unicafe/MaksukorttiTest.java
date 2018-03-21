767a6c41eb77bcf22071c1d8c4ed909aa47b8217
hajame <harri.mehtala@gmail.com>
2018-03-21 16:16:55 +0200

    unicafea muokattu

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -23,4 +23,34 @@ public class MaksukorttiTest {
         assertEquals("saldo: 10.0", kortti.toString());
     }
     
+    @Test
+    public void saldoEiMuutuJosRahaaEiTarpeeksi() {
+        kortti.otaRahaa(500);
+        kortti.otaRahaa(1000);
+        
+        
+        assertEquals("saldo: 5.0", kortti.toString());
+    }
+    
+    @Test
+    public void palauttaaTruejosRahatRiittavat() {
+        assertTrue(kortti.otaRahaa(500));
+    }
+    
+    @Test
+    public void palauttaaFalsejosRahatEivätRiita() {
+        assertFalse(kortti.otaRahaa(1500));
+    }
+
+    @Test
+    public void antaaOikeanSaldon() {
+        assertEquals(1000, kortti.saldo());
+    }
+
+    @Test
+    public void  lisaaOikeanMaaran() {
+        kortti.lataaRahaa(100);
+        assertEquals(1100, kortti.saldo());
+    }
+    
 }
