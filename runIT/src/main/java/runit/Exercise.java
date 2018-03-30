2888915db59bf8e6ab094670efe5f5a0f9728b5e
hajame <harri.mehtala@gmail.com>
2018-03-30 14:28:19 +0300

    Excercise class modified

--- runIT/src/main/java/runit/Exercise.java
+++ runIT/src/main/java/runit/Exercise.java
@@ -3,10 +3,36 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package runit;
 
+import java.sql.Timestamp;
 
 class Exercise {
 
+    private Timestamp time;
+    private int hours;
+    private int minutes;
+    private int seconds;
+    private double avgSpeed;
+    private double distance;
+
+    public Exercise(Timestamp time, int hours, int minutes, int seconds, double distance) {
+        this.time = time;
+        this.hours = hours;
+        this.minutes = minutes;
+        this.seconds = seconds;
+        this.distance = distance;
+        this.avgSpeed = this.distance / durationAsHours();
+    }
+
+
+    
+    public double durationAsHours() {
+    
+        
+        return 1.0 * (this.hours + this.minutes / 60 + this.seconds / 60 / 60);
+    }
+    
+    
+    
 }
