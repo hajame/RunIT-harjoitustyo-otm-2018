99abacbe08b050722e5b07a72d3883ab00c5a448
hajame <harri.mehtala@gmail.com>
2018-04-23 16:17:54 +0300

    edited GUI

--- runIT/src/main/java/runit/ui/GUI.java
+++ runIT/src/main/java/runit/ui/GUI.java
@@ -218,7 +218,7 @@ public class GUI extends Application {
         });
 
         // seutp primary stage
-        primaryStage.setTitle("Exercises");
+        primaryStage.setTitle("runIT");
         primaryStage.setScene(loginScene);
         primaryStage.show();
         primaryStage.setOnCloseRequest(e -> {
